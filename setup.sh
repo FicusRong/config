c8ea1c843bab96a0c542a0e81b1024f4565753e0
Shu Rong <srong@yitu-inc.intra>
2019-10-24 16:44:02 +0800

    update

--- setup.sh
+++ setup.sh
@@ -7,12 +7,15 @@ cp ./config/.profile ./
 git clone https://github.com/erikw/tmux-powerline.git
 rm ~/tmux-powerline/themes/default.sh
 ln -s ~/config/tmux/default.sh ~/tmux-powerline/themes/default.sh
+rm .tmux.conf
 ln -s ~/config/tmux/.tmux.conf
 
 mkdir .vim/bundle -p
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+rm .zshrc
+ln -s ~/config/.zshrc
 rm .vimrc
 ln -s ~/config/.vimrc
 
 EOF
-
+scp -r ~/.oh-my-zsh $ip:~/
